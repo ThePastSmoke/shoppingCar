d820f7f504d792caa0691c0bcdc0fe50a41e2dd8
Gzppp <2213041225@qq.com>
2022-06-26 17:59:22 +0800

    '完成购物车案例'

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>11</div>
+  <div class="app">
+    <MyHeader />
+    <MyGoods v-for="item in goodsList" :key="item.goods_id" :goods="item" />
+    <MyFooter :goodsList="goodsList" />
+  </div>
 </template>
 
 <script>
-export default {}
+import MyHeader from '#/MyHeader'
+import MyGoods from '#/MyGoods'
+import MyFooter from '#/MyFooter'
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      goodsList: []
+    }
+  },
+  components: {
+    MyHeader,
+    MyGoods,
+    MyFooter
+  },
+  async created() {
+    const res = await axios({ url: '/api/cart' })
+    this.goodsList = res.data.list
+  }
+}
 </script>
 
-<style></style>
+<style>
+.app {
+  padding: 50px 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 引入第三方库bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+// 引入axios插件
+import axios from 'axios'
+axios.defaults.baseURL = 'https://applet-base-api-t.itheima.net'
+axios.defaults.timeout = 3000
 Vue.config.productionTip = false
 new Vue({
   render: (h) => h(App)
